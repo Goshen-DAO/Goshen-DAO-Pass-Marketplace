57bbd3f71d9660fabec703c23885785ed91034c2
dzypherit <31358319+dzypherit@users.noreply.github.com>
2023-12-26 08:12:17 +0800

    fixed build error on .env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: "",
-};
+// Load environment variables from .env file in development and on Vercel
+if (process.env.NODE_ENV !== 'production' || process.env.VERCEL) {
+  const result = require('dotenv').config();
+  if (result.error) {
+    throw result.error;
+  }
+}
 
-module.exports = nextConfig;
+module.exports = {
+  basePath: '',
+  reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+  env: {
+    THIRDWEB_API_KEY: process.env.THIRDWEB_API_KEY,
+    CREST_SMARTWALLET_FACTORY_ADDRESS: process.env.CREST_SMARTWALLET_FACTORY_ADDRESS,
+  },
+};
